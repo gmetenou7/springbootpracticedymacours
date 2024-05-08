6a3160b0caf3f124229b7b77b0c734eb64ac2c30
NGUEKENG METENOU GILDAS <gildasmetenou@MBP-de-NGUEKENG.lan>
2024-05-08 13:45:13 +0200

    first commit for practice springboot dyma cours

---
+++ src/main/java/com/dyma/springpracticecours/SpringpracticecoursApplication.java
@@ -0,0 +1,13 @@
+package com.dyma.springpracticecours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpracticecoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringpracticecoursApplication.class, args);
+	}
+
+}
